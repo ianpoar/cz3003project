f19d939bf450408da8bbc23bdf34720f97faccf0
ianpoar <ianpoar@hotmail.com>
2020-09-30 22:36:46 +0800

    removed duplicate question class

--- EscapeNLearn/Assets/Source Code/Boundary/GameScreen.cs
+++ EscapeNLearn/Assets/Source Code/Boundary/GameScreen.cs
@@ -3,16 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Question
-{
-    public string question;
-    public string answer1;
-    public string answer2;
-    public string answer3;
-    public string answer4;
-    public int correctanswer;
-}
-
 public class GameScreen : Screen
 {
     public GameMgr GameManager;
