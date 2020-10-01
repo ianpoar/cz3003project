72045408799775875735e096f4e002d78f4a8657
Rakshitha Arun <35726675+RakshithaArun@users.noreply.github.com>
2020-10-02 01:59:01 +0800

    Update SettingsScreen.cs

--- EscapeNLearn/Assets/Source Code/Boundary/SettingsScreen.cs
+++ EscapeNLearn/Assets/Source Code/Boundary/SettingsScreen.cs
@@ -1,3 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+public class SettingsScreen : MonoBehaviour
+{
+
+    public float BGMvol=1f;
+    public float SFXvol=1f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        AudioMgr.Instance.SetBGMVol(BGMvol);
+        AudioMgr.Instance.SetSFXVol(SFXvol);
+    }
+
+    public void SetBGMVol(float vol)
+    {
+       BGMvol=vol;
+    }
+    public void SetSFXVol(float vol)
+    {
+       SFXvol=vol;
+    }
+    
+}
