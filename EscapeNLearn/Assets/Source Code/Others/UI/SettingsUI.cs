1350e3490d51ed3e8af78cb15a444642347f226c
ianpoar <ianpoar@hotmail.com>
2020-10-02 11:41:29 +0800

    Merged menu.unity - Rakshitha
    Settings done

---
+++ EscapeNLearn/Assets/Source Code/Others/UI/SettingsUI.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsUI : MonoBehaviour
+{
+    public Slider SFXSlider;
+    public Slider BGMSlider;
+    float BGMvol=1f;
+    float SFXvol=1f;
+
+    void OnEnable()
+    {
+        if (PlayerPrefs.HasKey(PPConstants.KEY_BGMVOL))
+        {
+            BGMvol = PlayerPrefs.GetFloat(PPConstants.KEY_BGMVOL);
+        }
+
+        if (PlayerPrefs.HasKey(PPConstants.KEY_SFXVOL))
+        {
+            SFXvol = PlayerPrefs.GetFloat(PPConstants.KEY_SFXVOL);
+        }
+
+        SFXSlider.value = SFXvol;
+        BGMSlider.value = BGMvol;
+    }
+
+    void OnDisable()
+    {
+        PlayerPrefs.SetFloat(PPConstants.KEY_BGMVOL, BGMvol);
+        PlayerPrefs.SetFloat(PPConstants.KEY_SFXVOL, SFXvol);
+    }
+
+    public void SetBGMVol(float vol)
+    {
+        BGMvol=vol;
+        AudioMgr.Instance.SetBGMVol(BGMvol);
+    }
+    public void SetSFXVol(float vol)
+    {
+       SFXvol=vol;
+        AudioMgr.Instance.SetSFXVol(SFXvol);
+    }
+    
+}
